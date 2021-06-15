cff8ca88844775ed3cf1e393bc23c9d6ea0069c1
Anton Christoffersson <anton_christofferssonhotmail.com>
2021-06-15 19:43:32 +0200

    map progress, texture not working..

--- src/MapLoader/MapLoader.cpp
+++ src/MapLoader/MapLoader.cpp
@@ -5,7 +5,9 @@ MapLoader::MapLoader(ShaderProgram &shader) : m_modelMat(1.0f), m_textureMat(1.0
                          m_quad(m_modelMat, m_textureMat),
                          m_texture(1), GraphicsObject(shader) { //TODO: Maybe have a texture mgr?
 
-    m_texture.setTextureData(m_mapData, 10, 10); //TODO: This isn't correct..
+    m_width = 10;
+    m_height = 10;
+    parseMap();
     setVertexData(sizeof(m_vertices), m_vertices);
     setIndexData(sizeof(m_indices), m_indices);
 }
@@ -16,6 +18,23 @@ MapLoader::~MapLoader() {}
 void MapLoader::draw() {
     m_shader.use();
     bindVAO();
+    glUniform1i(m_shader.getUniformLocation("useTexture"), 1);
     m_texture.bind();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void MapLoader::parseMap() {
+    unsigned char* texData;
+    texData = (unsigned char *) malloc(sizeof(unsigned char) * m_width * m_width);
+    for (size_t i = 0; i < m_width*m_width; i++) {
+        switch (m_mapData[i]) {
+        case tileType::ground:
+            texData[i] = 0;
+            break;
+        case tileType::wall:
+            texData[i] = 255;
+            break;
+        }
+    }
+    m_texture.setTextureData(texData, 10, 10); 
 }
--- src/MapLoader/MapLoader.hpp
+++ src/MapLoader/MapLoader.hpp
@@ -13,13 +13,20 @@ public:
     void draw();
 private:
 
+    void parseMap();
     unsigned int m_VBO;
     Quad m_quad;
     Texture m_texture;
     SimpleShaderProgram m_shader;
     glm::mat4 m_modelMat;
     glm::mat4 m_textureMat;
-
+    
+    unsigned int m_width;
+    unsigned int m_height;
+    enum tileType {
+        ground,
+        wall
+    };
 
     unsigned char m_mapData [10*10] = {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -32,12 +39,12 @@ private:
         1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1
     };
-    float m_vertices[20] = {
-        //Position             Texture coords
-        -0.5f, -0.5f, 0.0f,    0.0f, 1.0f,
-        0.5f, -0.5f, 0.0f,     1.0f, 1.0f,
-        -0.5f,  0.5f, 0.0f,    0.0f, 0.0f,
-        0.5f,  0.5f, 0.0f,     1.0f, 0.0f,
+    float m_vertices[36] = {
+        //Position              Color                       Texture coords
+        -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f,     0.0f, 1.0f,
+        0.5f, -0.5f, 0.0f,      0.0f, 1.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f,     0.0f, 0.0f,
+        0.5f,  0.5f, 0.0f,      1.0f, 0.0f, 1.0f, 1.0f,     1.0f, 0.0f
     };
     unsigned int m_indices[6] = {
         0, 1, 2,
