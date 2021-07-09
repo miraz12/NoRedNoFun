3a0ea427606156acb6abf6266186141f195a6553
emisto <emil1stolpe@gmail.com>
2021-07-09 23:06:10 +0200

    separated declaration and defeintion of system

--- src/ECS/Systems/System.h
+++ src/ECS/Systems/System.h
@@ -9,71 +9,27 @@ class System {
 public:
 
 	//Created many different constructors for different amount of required components
-	System(ECSManager *ECSManager, ComponentTypeEnum c1)
-		: m_manager(ECSManager), m_requiredComponents{ c1 }, m_entities() {
-	}
-	System(ECSManager* ECSManager, ComponentTypeEnum c1, ComponentTypeEnum c2)
-		: m_manager(ECSManager), m_requiredComponents{ c1, c2 }, m_entities() {
-	}
-	System(ECSManager* ECSManager, ComponentTypeEnum c1, ComponentTypeEnum c2, ComponentTypeEnum c3)
-		: m_manager(ECSManager), m_requiredComponents{ c1, c2, c3 }, m_entities() {
-	}
+	System(ECSManager* ECSManager, ComponentTypeEnum c1);
+		
+	System(ECSManager* ECSManager, ComponentTypeEnum c1, ComponentTypeEnum c2);
+
+	System(ECSManager* ECSManager, ComponentTypeEnum c1, ComponentTypeEnum c2, ComponentTypeEnum c3);
+		
 
 	//Checks if entity has correct components
-	bool entityHasCorrectComponents(Entity* entity) {
-
-		//assumes entities has at most one of each type of component
-		for (int i = 0; i < m_requiredComponents.size(); ++i) {
-
-			if (!entity->hasComponent(m_requiredComponents[i])) {
-				return false; //Component does not exist!
-			}
-		}
-		return true;
-	}
+	bool entityHasCorrectComponents(Entity* entity);
 
 	//Adds entity to System if it contains the right components
-	bool addEntity(Entity* entity){
-
-		if (entityHasCorrectComponents(entity)) {
-
-			//the right components exists in entity
-			m_entities.push_back(entity);
-
-			return true;
-		}
-		return false;
-	}
+	bool addEntity(Entity* entity); 
 
 	//checks if entity belongs to this system
-	bool containsEntity(int entityID) {
-		for (auto& entity : m_entities) {
-			if (entity->m_ID == entityID) {
-				return true;
-			}
-		}
-		return false;
-	}
+	bool containsEntity(int entityID);
 
 	//finds entity by id and removes it if it does not contain correct components
-	void removeFaultyEntity(int ID) {
-		for (int i = m_entities.size() - 1; i >= 0; i--) {
-			if (m_entities[i]->m_ID == ID) {
-				if (!entityHasCorrectComponents(m_entities[i])) {
-					m_entities.erase(m_entities.begin() + i);
-				}
-			}
-		}
-	}
+	void removeFaultyEntity(int ID);
 
 	//Removes entity
-	void removeEntity(int ID) {
-		for (int i = m_entities.size() - 1; i >= 0; i--) {
-			if (m_entities[i]->m_ID == ID) {
-				m_entities.erase(m_entities.begin() + i);
-			}
-		}	
-	}
+	void removeEntity(int ID);
 
 	//this function is called when the system should do its thing
 	virtual void update(float dt) = 0;
