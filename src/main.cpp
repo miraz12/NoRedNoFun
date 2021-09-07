58917ecc1217b02ab34719a4b461689196b39f4b
Anton <anton_christoffersson@hotmail.com>
2021-09-07 12:54:29 +0200

    Added error handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include "Game/Window.hpp"
 
-int main() {
-    Window win;
-    if (win.open()) {
-        return win.gameLoop();
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+void handle_eptr(std::exception_ptr eptr) {
+    try {
+        if (eptr) {
+            std::rethrow_exception(eptr);
+        }
+    } catch(const std::exception& e) {
+        std::cout << "Caught exception \"" << e.what() << "\"\n";
     }
-    return -1;
+}    
+
+int main() {
+    std::exception_ptr eptr;
+    try {
+        Window win;
+        if (win.open()) {
+            return win.gameLoop();
+        }
+        return -1;
+    } catch(...) {
+        eptr = std::current_exception();
+    } 
+    handle_eptr(eptr);    
 }
 
